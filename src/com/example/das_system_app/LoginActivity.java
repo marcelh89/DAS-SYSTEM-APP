0604b8f803eca9479fd00a421ad2e9651a952d16
marcelh89 <marcelh89@googlemail.com>
2014-05-29 15:36:47 +0200

    register view

--- src/com/example/das_system_app/LoginActivity.java
+++ src/com/example/das_system_app/LoginActivity.java
@@ -1,14 +1,35 @@
 package com.example.das_system_app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+
+		Button loginButton = (Button) findViewById(R.id.LoginButton);
+		loginButton.setOnClickListener(this);
+
 	}
 
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.LoginRegistrieren:
+			startActivity(new Intent(this, RegisterActivity.class));
+			break;
+		case R.id.LoginButton:
+			startActivity(new Intent(this, RegisterActivity.class));
+			break;
+		default:
+		}
+
+	}
 }
---
+++ src/com/example/das_system_app/RegisterActivity.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.example.das_system_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+/**
+ * @author marcman
+ *
+ */
+public class RegisterActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register);
+
+		
+	}
+
+}
