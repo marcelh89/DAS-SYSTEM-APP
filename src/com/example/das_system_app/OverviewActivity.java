5c834961fc70b6d2501b381093c06e21faaf0cd7
marcelh89 <marcelh89@googlemail.com>
2014-06-02 18:35:30 +0200

    Accordion für Hauptauswahl

--- src/com/example/das_system_app/OverviewActivity.java
+++ src/com/example/das_system_app/OverviewActivity.java
@@ -3,59 +3,114 @@
  */
 package com.example.das_system_app;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.example.das_system_app.adapters.ExpandableListAdapter;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.Toast;
 
-/**
- * @author marcman
- * 
- */
-public class OverviewActivity extends Activity implements OnClickListener {
+public class OverviewActivity extends Activity {
+
+	List<String> groupList;
+	List<String> childList;
+	Map<String, List<String>> laptopCollection;
+	ExpandableListView expListView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.overview);
 
-		Button buttons[] = { (Button) findViewById(R.id.OverviewNavigation),
-				(Button) findViewById(R.id.OverviewChat),
-				(Button) findViewById(R.id.OverviewNutzerprofil),
-				(Button) findViewById(R.id.OverviewRauminfo),
-				(Button) findViewById(R.id.OverviewDozent) };
+		createGroupList();
 
-		for (Button button : buttons) {
-			button.setOnClickListener(this);
-		}
+		createCollection();
 
+		expListView = (ExpandableListView) findViewById(R.id.laptop_list);
+		final ExpandableListAdapter expListAdapter = new ExpandableListAdapter(
+				this, groupList, laptopCollection);
+		expListView.setAdapter(expListAdapter);
+
+		// setGroupIndicatorToRight();
+
+		expListView.setOnChildClickListener(new OnChildClickListener() {
+
+			public boolean onChildClick(ExpandableListView parent, View v,
+					int groupPosition, int childPosition, long id) {
+				final String selected = (String) expListAdapter.getChild(
+						groupPosition, childPosition);
+				Toast.makeText(getBaseContext(), selected, Toast.LENGTH_LONG)
+						.show();
+				
+				/*
+				 * switch-case für neue Intents
+				 * startActivity(new Intent(this, RegisterActivity.class));
+				 */
+
+				return true;
+			}
+		});
 	}
 
-	@Override
-	public void onClick(View view) {
+	private void createGroupList() {
+		groupList = new ArrayList<String>();
+		groupList.add("Navigation");
+		groupList.add("Chat");
+		groupList.add("Nutzerprofil");
+		groupList.add("Rauminfo");
+		groupList.add("Dozent");
+	}
 
-		switch (view.getId()) {
-		case R.id.OverviewNavigation:
-			startActivity(new Intent(this, NavigationActivity.class));
-			break;
-		case R.id.OverviewChat:
-			startActivity(new Intent(this, ChatActivity.class));
-			break;
-		case R.id.OverviewNutzerprofil:
-			startActivity(new Intent(this, ProfilActivity.class));
-			break;
-		case R.id.OverviewRauminfo:
-			startActivity(new Intent(this, RaumActivity.class));
-			break;
-		case R.id.OverviewDozent:
-			startActivity(new Intent(this, DozentActivity.class));
-			break;
-		default:
+	private void createCollection() {
+		// preparing laptops collection(child)
+		String[] navigationOptions = { "Freunde im Umkreis",
+				"Weg zum Freund finden" };
+		String[] chatOptions = { "globaler Chat", "Gruppenchat" };
+		String[] profilOptions = { "Profil einsehen" };
+		String[] raumOptions = { "Raum identifizieren",
+				"Für einen Kurs anmelden" };
+		String[] dozentOptions = { "Anwesenheit prüfen",
+				"QR-Code für LV bereitstellen" };
 
+		laptopCollection = new LinkedHashMap<String, List<String>>();
+
+		for (String laptop : groupList) {
+			if (laptop.equals("Navigation")) {
+				loadChild(navigationOptions);
+			} else if (laptop.equals("Chat"))
+				loadChild(chatOptions);
+			else if (laptop.equals("Nutzerprofil"))
+				loadChild(profilOptions);
+			else if (laptop.equals("Rauminfo"))
+				loadChild(raumOptions);
+			else
+				// (laptop.equals("Dozent"))
+				loadChild(dozentOptions);
+
+			laptopCollection.put(laptop, childList);
 		}
+	}
 
+	private void loadChild(String[] laptopModels) {
+		childList = new ArrayList<String>();
+		for (String model : laptopModels)
+			childList.add(model);
+	}
+
+	// Convert pixel to dip
+	public int getDipsFromPixel(float pixels) {
+		// Get the screen's density scale
+		final float scale = getResources().getDisplayMetrics().density;
+		// Convert the dps to pixels, based on density scale
+		return (int) (pixels * scale + 0.5f);
 	}
 
 }
