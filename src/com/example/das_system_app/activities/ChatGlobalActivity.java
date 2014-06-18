669e5153e6a02387ed0adaab31aef0d368928f42
MaRcMaN <marcelh89@googlemail.com>
2014-06-18 18:11:30 +0200

    working connection websocket

--- src/com/example/das_system_app/activities/ChatGlobalActivity.java
+++ src/com/example/das_system_app/activities/ChatGlobalActivity.java
@@ -3,9 +3,12 @@ package com.example.das_system_app.activities;
 import java.util.HashMap;
 
 import com.example.das_system_app.R;
-
+import de.tavendo.autobahn.WebSocketConnection;
+import de.tavendo.autobahn.WebSocketException;
+import de.tavendo.autobahn.WebSocketHandler;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
@@ -17,6 +20,9 @@ public class ChatGlobalActivity extends Activity implements OnClickListener {
 	ImageButton postbtn;
 	TextView chatView;
 	EditText inputField;
+
+	private static String CLS = "ChatService";
+	private final WebSocketConnection mConnection = new WebSocketConnection();
 
 	// HashMap<String, String> chatlog;
 
@@ -33,6 +39,8 @@ public class ChatGlobalActivity extends Activity implements OnClickListener {
 		postbtn = (ImageButton) findViewById(R.id.imageButton1);
 		postbtn.setOnClickListener(this);
 
+		connect();
+
 	}
 
 	@Override
@@ -47,5 +55,34 @@ public class ChatGlobalActivity extends Activity implements OnClickListener {
 			inputField.setText("");
 		}
 
+	}
+
+	public void connect() {
+
+		final String wsuri = "ws://192.168.178.60:8080/DAS-SYSTEM-SERVER/chat";
+
+		try {
+			mConnection.connect(wsuri, new WebSocketHandler() {
+
+				@Override
+				public void onOpen() {
+					Log.d(CLS, "Status: Connected to " + wsuri);
+					mConnection.sendTextMessage("Hello, world!");
+				}
+
+				@Override
+				public void onTextMessage(String payload) {
+					Log.d(CLS, "Got echo: " + payload);
+				}
+
+				@Override
+				public void onClose(int code, String reason) {
+					Log.d(CLS, "Connection lost.");
+				}
+			});
+		} catch (WebSocketException e) {
+
+			Log.d(CLS, e.toString());
+		}
 	}
 }
