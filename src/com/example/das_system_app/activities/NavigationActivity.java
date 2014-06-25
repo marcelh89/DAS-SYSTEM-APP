0fef9256a90859e655d814c1cdedce65da9d9af1
MaRcMaN <marcelh89@googlemail.com>
2014-06-25 23:23:47 +0200

    google maps api

--- src/com/example/das_system_app/activities/NavigationActivity.java
+++ src/com/example/das_system_app/activities/NavigationActivity.java
@@ -8,20 +8,26 @@ import com.example.das_system_app.R.layout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 /**
  * @author marcman
  * 
  */
-//TODO Implementation
-//https://developers.google.com/maps/documentation/android/v1/hello-mapview
-public class NavigationActivity extends Activity  {
+// TODO Implementation
+// https://developers.google.com/maps/documentation/android/v1/hello-mapview
+public class NavigationActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.navigation);
-
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
 }
