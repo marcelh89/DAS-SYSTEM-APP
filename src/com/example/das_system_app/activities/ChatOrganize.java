32dd90db929f1fdda18cdbe9c0b9419ca4858308
MaRcMaN <marcelh89@googlemail.com>
2014-07-03 16:13:44 +0200

    gruppe löschen außer global

--- src/com/example/das_system_app/activities/ChatOrganize.java
+++ src/com/example/das_system_app/activities/ChatOrganize.java
@@ -8,15 +8,19 @@ import com.example.das_system_app.R;
 import com.example.das_system_app.rest.valueobject.User;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -30,10 +34,14 @@ import android.widget.TextView;
  * @author marcman
  * 
  */
-public class ChatOrganize extends Activity implements OnItemClickListener {
+public class ChatOrganize extends Activity implements OnItemClickListener,
+		OnItemLongClickListener {
 
 	public static final String GROUP_NAME = "gName";
 	public static final String GROUP_PASSWORD = "gPassword";
+
+	public static final String OPTION_DELETE_STR = "Gruppe löschen";
+	public static final String OPTION_DETAIL_STR = "Details ansehen";
 
 	public static final int GROUP_CREATE = 0;
 	public static final int FRIEND_INVITE = 1;
@@ -60,6 +68,9 @@ public class ChatOrganize extends Activity implements OnItemClickListener {
 		listView.setAdapter(dataAdapter);
 
 		listView.setOnItemClickListener(this);
+		listView.setOnItemLongClickListener(this);
+
+		registerForContextMenu(listView);
 
 		new Handler().postDelayed(new Runnable() {
 			public void run() {
@@ -79,14 +90,32 @@ public class ChatOrganize extends Activity implements OnItemClickListener {
 
 		Intent intent = new Intent(this, ChatActivity.class);
 
-		// if (room != "global") {
-		// intent.putExtra("isPrivate", true);
-		// }
-
 		intent.putExtra("room", room);
 		intent.putExtra("user", currentUser);
 		startActivity(intent);
 
+	}
+
+	/**
+	 * longclicklistener fuer löschen, detailansicht von chats
+	 */
+	@Override
+	public boolean onItemLongClick(AdapterView<?> parent, View view,
+			int position, long id) {
+
+		TextView c = (TextView) view.findViewById(R.id.rowTextView);
+		String room = c.getText().toString();
+		Log.i("OnItemLongClicked", room);
+
+		startSelectionDialog(position);
+
+		// Intent intent = new Intent(this, ChatDetailActivity.class);
+
+		// intent.putExtra("room", room);
+		// intent.putExtra("user", currentUser);
+		// startActivity(intent);
+
+		return false;
 	}
 
 	@Override
@@ -134,4 +163,68 @@ public class ChatOrganize extends Activity implements OnItemClickListener {
 
 		}
 	}
+
+	private int startSelectionDialog(final int position) {
+
+		AlertDialog.Builder ad = new AlertDialog.Builder(this);
+		ad.setIcon(R.drawable.ic_launcher);
+		ad.setTitle("Was wollen Sie tun?");
+		// ad.setMessage("Die Verbindung zum Server konnte nicht hergestellt werden, "
+		// + "Klick ok um zurück zur Hauptansicht zu gelangen");
+		ad.setCancelable(true);
+		ad.setNegativeButton(OPTION_DELETE_STR,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+						if (position == 0) {
+							startFailDialog();
+						} else {
+							chatlist.remove(position);
+							dataAdapter.notifyDataSetChanged();
+						}
+
+					}
+				});
+		ad.setNeutralButton(OPTION_DETAIL_STR,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+						// Intent intent = new Intent(this,
+						// ChatDetailActivity.class);
+
+						// intent.putExtra("room", room);
+						// intent.putExtra("user", currentUser);
+						// startActivity(intent);
+
+					}
+				});
+
+		AlertDialog alert = ad.create();
+		alert.show();
+
+		return 0;
+
+	}
+
+	private int startFailDialog() {
+
+		AlertDialog.Builder ad = new AlertDialog.Builder(this);
+		ad.setIcon(R.drawable.ic_launcher);
+		ad.setTitle("Fehler beim Löschen");
+		ad.setMessage("Die globale Gruppe kann nicht gelöscht werden !");
+		ad.setCancelable(true);
+		ad.setNegativeButton(android.R.string.ok,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+
+		AlertDialog alert = ad.create();
+		alert.show();
+
+		return 0;
+
+	}
+
 }
